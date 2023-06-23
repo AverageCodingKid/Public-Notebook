c0bc063ce13b96df3f3caac748bc95f52d40c8f5
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-06-23 08:15:16 -0400

    NoteBook

---
+++ Note_1.py
@@ -0,0 +1,5 @@
+# Note 1
+number_1 = float(input("Enter a number:"))
+number_2 = float(input("Enter a number:"))
+print(number_1 + number_2)
+# Adds the numbers that are typed
---
+++ Note_2.py
@@ -0,0 +1,5 @@
+# Note 2
+user_float = float(input("Enter an integer:"))
+print(user_float)
+print(type(user_float))
+print(int(1.6))
