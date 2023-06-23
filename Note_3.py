8edc6ac2e863bdc584d75bc6cf66cfc81c126ba0
shealewis123 <136538818+shealewis123@users.noreply.github.com>
2023-06-23 15:42:27 -0400

    note_3

---
+++ Note_3.py
@@ -0,0 +1,8 @@
+def clock():
+    print("What time is it?")
+
+    time = input()
+    print("The time right now is " + time)
+clock()
+
+# Asks what time it is to format The time right now is + the time entered
